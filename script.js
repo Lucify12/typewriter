88a91a7e27ae5ea9b53ffef0a43cb60f0c6306d1
Lucify <64998119+Lucify12@users.noreply.github.com>
2023-02-05 22:40:13 -0500

    Removed some useless code and added comments

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ let index = 0;
 let charindex = 0;
 const speed = 200;
 
+console.log(phrases.length)
+
 function typeWriter() {
     let char = phrases[index].charAt(charindex)
-    let currentWord = phrases
+
+    // pushes the letter of the first index and increments by 1
+
     if (0 < phrases[index].length) {
         e.push(char)
         container.innerHTML = e.join('');
         charindex++;
-        setTimeout(typeWriter, 225);
+        setTimeout(typeWriter, 250);
     }
+
+    // increments the pharases index by one which will just keep going to the next word
 
     if (phrases[index].length === e.length) {
         charindex = 0;
         e = []
         index++;
     }
+
+    // so that the array will loop
 
     if (index === phrases.length) {
         index = 0
